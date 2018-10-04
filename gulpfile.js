bbf16a7357c4949e45f17617f9cf3231a2d9f794
dominik <dominik.obuch@wp.pl>
2018-10-04 21:04:22 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var del = require('del');
+var runSequence = require('run-sequence');
+var gutil = require('gulp-util');
+var concat = require('gulp-concat');
+var babili = require('gulp-babili');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('browserSync', () => {
+  browserSync.init({
+    open: false,
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+
+gulp.task('sass', () => {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], () => {
+  gulp.watch('app/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('useref', () => {
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+
+
+gulp.task('scripts', () => {
+  return gulp.src(['app/js/*.js'])
+    .pipe(concat('index.min.js'))
+    .pipe(babili({
+      mangle: {
+        keepClassNames: true
+      }
+    }))
+  .on('error', function (err) {
+     gutil.log(gutil.colors.red('[Error]'), err.toString());
+   })
+  .pipe(gulp.dest('dist/js'));
+})
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('app/js/*.js'),
+        uglify(),
+        gulp.dest('dist/js')
+    ],
+    cb
+  );
+});
+
+gulp.task('prefixer', () =>
+    gulp.src('app/css/*.css')
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('app/css'))
+);
+
+
+gulp.task('clean:dist', () => {
+  return del.sync('dist');
+})
+
+gulp.task('default', function (callback) {
+  runSequence(['watch', 'sass', 'browserSync'],
+    callback
+  )
+})
+
+gulp.task('build', function (callback) {
+  runSequence('clean:dist', ['default', 'images'] ,'prefixer', 'useref','scripts','compress' ,
+    callback)
+})
