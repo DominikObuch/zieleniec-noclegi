b9fe3ba14e02c34653770669b1437b8b6e686c63
dominik <dominik.obuch@wp.pl>
2018-10-11 12:28:51 +0200

    header changed

--- app/js/index.js
+++ app/js/index.js
@@ -19,61 +19,6 @@ hamburger.addEventListener("click", () => {
 
 })
 
-//slider js
-let sliders = document.getElementsByClassName("slider__image--pic");
-let sliderActive = null;
-for (let i = 0; i < sliders.length; i++) {
-    if (sliders[i].classList.contains("slider__image--pic-active")) {
-        sliderActive = i;
-    }
-}
-document.getElementsByClassName("slider__arrow--right")[0].addEventListener("click", () => {
-
-    sliders[sliderActive].classList.toggle("sliderOutLeft")
-    let asyncSliderActive = sliderActive;
-    sliders[sliderActive].classList.remove("slider__image--pic-active")
-    sliders[sliderActive].addEventListener("animationend", () => {
-        console.log(asyncSliderActive + "async")
-        sliders[asyncSliderActive].classList.remove("sliderOutLeft");
-
-    })
-    if (sliderActive == 2) {
-        sliderActive = 0;
-    } else {
-        sliderActive++;
-    }
-    sliders[sliderActive].classList.add("slider__image--pic-active", "sliderInLeft");
-    let asyncSliderActiveTwo = sliderActive;
-    sliders[sliderActive].addEventListener("animationend", remAnimSecond = () => {
-
-        sliders[asyncSliderActiveTwo].classList.remove("sliderInLeft")
-    })
-})
-
-
-
-document.getElementsByClassName("slider__arrow--left")[0].addEventListener("click", () => {
-
-    sliders[sliderActive].classList.toggle("sliderOutRight")
-    let asyncSliderActive = sliderActive;
-    sliders[sliderActive].classList.remove("slider__image--pic-active")
-    sliders[sliderActive].addEventListener("animationend", () => {
-        console.log(asyncSliderActive + "async")
-        sliders[asyncSliderActive].classList.remove("sliderOutRight");
-
-    })
-    if (sliderActive == 0) {
-        sliderActive = 2;
-    } else {
-        sliderActive--;
-    }
-    sliders[sliderActive].classList.add("slider__image--pic-active", "sliderInRight");
-    let asyncSliderActiveTwo = sliderActive;
-    sliders[sliderActive].addEventListener("animationend", remAnimSecond = () => {
-
-        sliders[asyncSliderActiveTwo].classList.remove("sliderInRight")
-    })
-})
 
 //main 
 let article = document.getElementsByClassName("article__image");
