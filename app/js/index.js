81359545d45616e42d6352ef2dc4b43e6677f42d
dominik <dominik.obuch@wp.pl>
2018-10-19 16:23:25 +0200

    final version

--- app/js/index.js
+++ app/js/index.js
@@ -1,37 +1,38 @@
 //hamburger js 
 let hamburger = document.getElementsByClassName("hamburger")[0];
 let hamburgerState = false;
-document.body.addEventListener("click", () => {
-    console.log(event.target);
-    if (hamburgerState && event.target.classList[0].substr(0, 9) !== "hamburger" && event.target.classList[0] !== "header__list") {
-        document.getElementsByClassName("header__list")[0].classList.toggle("header__list--invisible");
-        document.getElementsByClassName("header__list")[0].classList.toggle("header__list--visible");
-        hamburgerState = !hamburgerState;
-
-    }
+document.addEventListener("click", function () {
+    console.log(event.target)
 });
-
 let hamburgerBg = document.createElement("div");
 hamburgerBg.classList.add("hamburger__background");
+
 hamburger.addEventListener("click", () => {
-
     hamburger.classList.toggle("is-active");
-
     if (hamburgerState) {
 
-        document.body.removeChild(document.getElementsByClassName("hamburger__background")[0]);
 
+        document.body.removeChild(document.getElementsByClassName("hamburger__background")[0]);
         document.getElementsByClassName("header__list")[0].classList.toggle("header__list--visible");
         document.getElementsByClassName("header__list")[0].classList.toggle("header__list--invisible");
+
     } else {
         document.body.appendChild(hamburgerBg);
+        document.getElementsByClassName("hamburger__background")[0].addEventListener("click", () => {
+            if (hamburgerState && document.getElementsByClassName("hamburger__background")[0]) {
+                document.getElementsByClassName("header__list")[0].classList.toggle("header__list--invisible");
+                document.getElementsByClassName("header__list")[0].classList.toggle("header__list--visible");
+                hamburgerState = !hamburgerState;
+                document.body.removeChild(document.getElementsByClassName("hamburger__background")[0])
+            }
+        });
         document.getElementsByClassName("header__list")[0].classList.remove("header__list--invisible");
         document.getElementsByClassName("header__list")[0].classList.toggle("header__list--visible");
+
     }
     hamburgerState = !hamburgerState;
 
 })
-
 
 //main 
 let article = document.getElementsByClassName("article__image");
